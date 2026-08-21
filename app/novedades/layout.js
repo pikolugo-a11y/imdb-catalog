3fc7cc3013737467c19650d35bfd7d86887717b9
pikolugo-a11y <pikolugo@gmail.com>
2026-08-21 22:18:04 +0200

    fix: eliminar duplicado Plex en Novedades (#104)

--- app/novedades/layout.js
+++ app/novedades/layout.js
@@ -1,8 +1,3 @@
-import NovedadesPlexShell from './NovedadesPlexShell';
-import PlexIntake from './PlexIntake';
-import './plex-intake.css';
-
 export default function Layout({children}){
-  const intake=<PlexIntake/>;
-  return <NovedadesPlexShell intake={intake}>{children}</NovedadesPlexShell>;
+  return children;
 }
