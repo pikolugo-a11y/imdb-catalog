16d04379ad9f919a2df5bf922fcd493017f3218f
pikolugo-a11y <pikolugo@gmail.com>
2026-08-21 23:38:05 +0200

    Remove obsolete FilmAffinity ambiguities page (#113)
    * Remove FilmAffinity ambiguos navigation
    
    * Remove obsolete FilmAffinity ambiguos actions
    
    * Remove obsolete FilmAffinity ambiguos page

--- app/calidad/identidad/layout.js
+++ app/calidad/identidad/layout.js
@@ -1,2 +1,1 @@
-import Link from 'next/link';
-export default function IdentityLayout({children}){return <><div style={{display:'flex',gap:8,flexWrap:'wrap',marginBottom:14}}><Link className="button ghost" href="/calidad/identidad">Identidad</Link><Link className="button ghost" href="/calidad/identidad/ambiguos">⚑ Ambiguos FilmAffinity</Link></div>{children}</>}
+export default function IdentityLayout({children}){return <>{children}</>}
