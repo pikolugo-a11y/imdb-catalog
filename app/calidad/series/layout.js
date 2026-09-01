48a2b082ce04921f8c1fe0b9c77c91625ebe79f6
pikolugo-a11y <pikolugo@gmail.com>
2026-09-01 17:50:48 +0200

    Batch V1: integrar acordeón en Calidad Series

---
+++ app/calidad/series/layout.js
@@ -0,0 +1,3 @@
+import SeriesBatchPanel from '@/components/SeriesBatchPanel';
+import '../datos/batch-panel.css';
+export default function SeriesLayout({children}){return <><SeriesBatchPanel/>{children}</>}
